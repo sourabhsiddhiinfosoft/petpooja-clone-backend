d6d67717311b55c692a5faff13ce22506f6cd945
sourabhsiddhiinfosoft <sourabh.siddhiinfosoft@gmail.com>
2025-09-28 19:18:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import { connectDB } from "./src/config/db.js";
 const PORT = process.env.PORT || 4000;
 
 const start = async () => {
-  await connectDB(process.env.MONGODB_URI);
+  //await connectDB(process.env.MONGODB_URI);
+   await connectDB("mongodb+srv://sourabh:sourabh123@localcluster0.0ri1ozc.mongodb.net/?retryWrites=true&w=majority&appName=LocalCLuster0");
   app.listen(PORT, () => console.log(`Running Server on http://localhost:${PORT}`));
 };
 
